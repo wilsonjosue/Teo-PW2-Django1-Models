20cc31c6c575cfe55dc8297f5cbc0c50c65f65c5
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-08 12:05:42 -0500

    Se importa los modelos, formularios y otros de tipo django

--- personas/views.py
+++ personas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegistroUsuarioForm, PersonaForm
+from .models import Persona
 # Create your views here.
+
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, 'register.html', {'form': form})
