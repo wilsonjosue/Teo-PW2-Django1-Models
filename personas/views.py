a812568b69829e9460644c10c14c8b2b30ed12d9
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-08 12:17:13 -0500

    Se crea la funcion login para ingresar el usuario  personas/views.py

--- personas/views.py
+++ personas/views.py
@@ -16,3 +16,15 @@ def registro(request): # Se implementa el registro para el usuario nuevo en un h
     else:
         form = RegistroUsuarioForm()
     return render(request, 'register.html', {'form': form})
+
+def login_view(request): # Se implementa la funcion login
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
