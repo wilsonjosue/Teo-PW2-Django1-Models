2f9f31cf90ec28c904ace1892b96a2d18712666c
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-08 15:38:23 -0500

    Se añade una nueva ruta logout para cerrar sesion. En personas/urls.py

--- personas/urls.py
+++ personas/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [ # Urls para redireccionar
     path('login/', views.login_view, name='login'),#Ruta para el login
     path('register/', views.registro, name='register'),#Ruta para registrar
     path('agregar/', views.agregar_persona, name='agregar_persona'),# Ruta para agregar persona
+    path('logout/', views.logout_view, name='logout'),#Ruta para cerrar sesion.
 ]
