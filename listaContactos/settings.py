76ba55fc43ba9e300710e4ac61f22390fe24ce26
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-08 14:00:21 -0500

    Se trata de corregir el error del no reconocimiento de templates, añadiendo la importacion os y en TEMPLATES un nuevo DIRS. En personas/migrations

--- listaContactos/settings.py
+++ listaContactos/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'listaContactos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
