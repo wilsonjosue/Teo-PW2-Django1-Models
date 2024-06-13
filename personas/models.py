59a95409950fef188190b315842ad098bf416052
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-13 00:01:45 -0500

    Se modifica el mmodelo y se agrega el campo donador boolean. La implementacion se realizo en personas/models.py, paso de diapositiva 4

--- personas/models.py
+++ personas/models.py
@@ -5,6 +5,7 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.DecimalField(max_digits=3, decimal_places=0)
+    donador = models.BooleanField()
 
     def __str__(self):
         return f'{self.nombre} {self.apellido} - Edad: {self.edad}'
